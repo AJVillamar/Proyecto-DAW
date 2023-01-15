4f94f3d20c6ff31fa0d455c7ce836b2e3c435d4b
AJVillamar <angelo.villamarm@ug.edu.ec>
2023-01-14 19:37:21 -0500

    v-9

--- src/app/Components/adopcion/adopcion.component.ts
+++ src/app/Components/adopcion/adopcion.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AnimalService } from 'src/app/Services/animal.service';
+import { IAnimal } from './../../Interfaces/IAnimal';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-adopcion',
   templateUrl: './adopcion.component.html',
-  styleUrls: ['./adopcion.component.css']
+  styleUrls: ['./adopcion.component.css'],
 })
-export class AdopcionComponent {
+
+export class AdopcionComponent implements OnInit {
+  animals!: IAnimal[];
+  selectedButton: string = 'Perros';
+
+  constructor(private animalService: AnimalService) { }
+
+  ngOnInit() {
+    this.animals = this.animalService.getDogs();
+  }
+
+  onButtonClick(button: string) {
+    this.selectedButton = button;
+    if (this.selectedButton === 'Perros') {
+      this.animals = this.animalService.getDogs();
+    } else {
+      this.animals = this.animalService.getCats();
+    }
+  }
+
 
 }
--- src/app/Services/animal.service.ts
+++ src/app/Services/animal.service.ts
@@ -32,6 +32,31 @@ export class AnimalService {
       id: 5, imagen: "dog5.jpg", nombre: "Daisy", raza: "Poodle", categoria: "Perro", 
       fechaNac: "15/06/2019", fechaIng: "01/02/2022", sexo: "Hembra", estado: "Adoptado",
       observacion: "Daisy fue rescatada de una situación de maltrato en una finca cerca de Loja. Fue encontrada atada a un poste sin acceso a agua ni comida. Después de unas semanas de cuidados, Daisy se recuperó y demostró ser una perra muy cariñosa y agradecida.",
+    },
+    {
+      id: 6, imagen: "gato1.jpg", nombre: "Mittens", raza: "Siamese", categoria: "Gato",
+      fechaNac: "01/01/2019", fechaIng: "01/06/2021", sexo: "Hembra", estado: "Disponible",
+      observacion: "Mittens fue encontrada en las calles de Quito, estaba muy delgada y necesitaba atención veterinaria urgente. Después de unas semanas de cuidados, Mittens se recuperó y demostró ser un gato muy cariñoso y juguetón.",
+    },
+    {
+      id: 7, imagen: "gato2.jpg", nombre: "Whiskers", raza: "Tabby", categoria: "Gato",
+      fechaNac: "15/06/2019", fechaIng: "01/06/2021", sexo: "Macho", estado: "Disponible",
+      observacion: "Whiskers fue encontrado vagando por las calles de Cuenca. Parecía haber sido abandonado por su antigua familia, así que decidimos llevarlo a la fundación. Es un gato muy activo y le encanta jugar con otros gatos y personas.",
+    },
+    {
+      id: 8, imagen: "gato3.jpg", nombre: "Luna", raza: "Persa", categoria: "Gato",
+      fechaNac: "05/04/2017", fechaIng: "15/08/2019", sexo: "Hembra", estado: "Esperando",
+      observacion: "Luna fue abandonada en una estación de tren en Guayaquil. Estaba muy asustada y desconfiada al principio, pero después de un poco de amor y atención, se convirtió en un gato muy cariñoso y leal.",
+    },
+    {
+      id: 9, imagen: "gato4.jpg", nombre: "Simba", raza: "Maine Coon", categoria: "Gato",
+      fechaNac: "20/05/2018", fechaIng: "05/11/2020", sexo: "Macho", estado: "Disponible",
+      observacion: "Simba fue encontrado por un trabajador del parque en el centro de la ciudad. Parecía haber estado allí por un tiempo y estaba muy delgado y sucio. Después de un poco de limpieza y comida, Simba se convirtió en un gato muy saludable y juguetón.",
+    },
+    {
+        id: 10, imagen: "gato5.jpg", nombre: "Bella", raza: "Siamese", categoria: "Gato",
+        fechaNac: "15/06/2019", fechaIng: "01/02/2022", sexo: "Hembra", estado: "Adoptado",
+        observacion: "Bella fue rescatada de una situación de maltrato en una finca cerca de Loja. Fue encontrada atada a un poste sin acceso a agua ni comida. Después de unas semanas de cuidados, Bella se recuperó y demostró ser una gata muy cariñosa y agradecida.",
     }
   ];
 
@@ -59,6 +84,14 @@ export class AnimalService {
 
   getAnimals(): IAnimal[] {
     return this.animal;
+  }
+
+  getDogs(): IAnimal[] {
+    return this.animal.filter(animal => animal.categoria === 'Perro');
+  }
+
+  getCats(): IAnimal[] {
+    return this.animal.filter(animal => animal.categoria === 'Gato');
   }
   
 }
