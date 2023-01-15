48710117e65fce32bf6a7d9dd30f57b59224d779
Henry7001 <genesissanchez_13@hotmail.com>
2023-01-15 17:53:48 -0500

    Denuncia beta 1

--- src/app/Shared/Inicio/denuncia-inicio/denuncia-inicio.component.ts
+++ src/app/Shared/Inicio/denuncia-inicio/denuncia-inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddRescateComponent } from 'src/app/Components/Administrador/rescate-admi/add-rescate/add-rescate.component';
 
 @Component({
   selector: 'app-denuncia-inicio',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./denuncia-inicio.component.css']
 })
 export class DenunciaInicioComponent {
+
+  constructor(public dialog: MatDialog) { }
+
+  openDialog() {
+    this.dialog.open(AddRescateComponent, {
+      width: '500px',
+      height: '500px'
+    });
+  }
 
 }
